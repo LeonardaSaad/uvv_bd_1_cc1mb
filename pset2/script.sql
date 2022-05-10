5ac0b1df864ce6fa2a80cb654686236df0dce5db
Leonarda Saad <67969095+LeonardaSaad@users.noreply.github.com>
2022-05-10 17:05:15 -0300

    Update script.sql

--- pset2/script.sql
+++ pset2/script.sql
@@ -1,9 +1,9 @@
--- 1) MÈdia salarial por departamento:
+-- 1) M√©dia salarial por departamento:
 SELECT numero_departamento, CAST(AVG(salario) as decimal(10,2))
 FROM elmasri.funcionario
 GROUP BY numero_departamento;
 
--- 2) MÈdia salarial dos homens e mulheres:
+-- 2) M√©dia salarial dos homens e mulheres:
 SELECT
 CASE sexo
 WHEN 'M' THEN 'Homens'
@@ -15,7 +15,7 @@ FROM elmasri.funcionario
 GROUP BY sexo;
 
 
--- 3) RelatÛrio dos departamentos com informaÁıes dos funcion·rios:
+-- 3) Relat√≥rio dos departamentos com informa√ß√µes dos funcion√°rios:
 SELECT
 	d.numero_departamento,
 	d.nome_departamento,
@@ -27,8 +27,8 @@ FROM elmasri.departamento d, elmasri.funcionario f;
 
 
 
--- 4) RelatÛrio com o nome completo, idade, salario atual e sal·rio com reajuste:
-   -- CritÈrio do reajuste: se o sal·rio atual for inferior a 35 mil, recebe um reajuste de 20% e se o sal·rio atual for igual ou	superior a 35.000, recebe reajuste de 15%.
+-- 4) Relat√≥rio com o nome completo, idade, salario atual e sal√°rio com reajuste:
+   -- Crit√©rio do reajuste: se o sal√°rio atual for inferior a 35 mil, recebe um reajuste de 20% e se o sal√°rio atual for igual ou	superior a 35.000, recebe reajuste de 15%.
 SELECT 
 CONCAT(f.primeiro_nome, ' ', f.nome_meio, ' ', f.ultimo_nome) AS nome_completo,
 DATE_PART('year', age(data_nascimento))::int AS idade,
@@ -38,7 +38,7 @@ salario *1.20 AS salario_reajustado
  WHERE salario < 35000 
  UNION
 SELECT 
-CONCAR(f.primeiro_nome, ' ', f.nome_meio, ' ', f.ultimo_nome) AS nome_completo,
+CONCAT(f.primeiro_nome, ' ', f.nome_meio, ' ', f.ultimo_nome) AS nome_completo,
 DATE_PART('year', age(data_nascimento))::int AS idade,
 salario AS salario_atual,
 salario *1.15 AS salario_reajustado
@@ -46,7 +46,7 @@ salario *1.15 AS salario_reajustado
  WHERE salario >= 35000;
 
 
--- 5) RelatÛrio que liste, para cada departamento colocar nome do gerente e o nome dos funcion·rios. Ordenar por nome departamento (em ordem crescente) e por sal·rio dos funcion·rios (em ordem decrescente).
+-- 5) Relat√≥rio que liste, para cada departamento colocar nome do gerente e o nome dos funcion√°rios. Ordenar por nome departamento (em ordem crescente) e por sal√°rio dos funcion√°rios (em ordem decrescente).
 WITH gerente AS (SELECT
 CONCAT(f.primeiro_nome, ' ', f.nome_meio, ' ', f.ultimo_nome) AS nome,
  	f.cpf 
@@ -62,7 +62,7 @@ CONCAT(f.primeiro_nome, ' ', f.nome_meio, ' ', f.ultimo_nome) AS nome_funcionari
  ORDER BY d.nome_departamento ASC, f.salario DESC;
 
 
--- 6) RelatÛrio que mostra o nome completo dos funcion·rios que tÍm dependente dependentes, o departamento onde eles trabalham, e para cada funcion·rio, liste o nome completo dos dependentes, a idade em anos de cada dependente e o sexo (sexo deve ser "Masculino" e "Feminino).
+-- 6) Relat√≥rio que mostra o nome completo dos funcion√°rios que t√™m dependente dependentes, o departamento onde eles trabalham, e para cada funcion√°rio, liste o nome completo dos dependentes, a idade em anos de cada dependente e o sexo (sexo deve ser "Masculino" e "Feminino).
 SELECT
 	CONCAT(f.primeiro_nome, ' ', f.nome_meio, ' ', f.ultimo_nome) AS funcionario,
 	d.nome_departamento AS departamento,
@@ -80,7 +80,7 @@ WHERE
 	(f.numero_departamento = d.numero_departamento);
 
 
--- 7)RelatÛrio que mostre, para cada funcion·rio que N√O TEM dependente, seu nome completo, departamento e sal·rio.
+-- 7)Relat√≥rio que mostre, para cada funcion√°rio que N√ÉO TEM dependente, seu nome completo, departamento e sal√°rio.
 SELECT DISTINCT
 	CONCAT(f.primeiro_nome, ' ', f.nome_meio, ' ', f.ultimo_nome) AS funcionarios,
 	f.numero_departamento AS numero_d,
@@ -94,7 +94,7 @@ WHERE EXISTS (SELECT *
 	f.numero_departamento=d.numero_departamento;
 
 
--- 8) RelatÛrio que mostre, para cada departamento os projetos e o nome completo dos funcion·rios alocados. AlÈm de incluir o n˙mero de horas trabalhadas por cada um.
+-- 8) Relat√≥rio que mostre, para cada departamento os projetos e o nome completo dos funcion√°rios alocados. Al√©m de incluir o n√∫mero de horas trabalhadas por cada um.
 SELECT
     d.nome_departamento AS departamentos,
     p.nome_projeto as projetos,
